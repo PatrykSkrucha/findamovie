c6347a40f29c1a23d546ba97590732290e5cdef8
PatrykSkrucha <patryk.skrucha@gmail.com>
2019-04-02 16:23:43 +0200

    Updates

---
+++ precache-manifest.37fe1a20551ec912b916b428334d0af1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f2131f00fc2297342b8",
+    "url": "/findamovie/static/js/runtime~main.9312300c.js"
+  },
+  {
+    "revision": "22ee8baddb42141e24b6",
+    "url": "/findamovie/static/js/main.92fa866d.chunk.js"
+  },
+  {
+    "revision": "be06475a052e14067896",
+    "url": "/findamovie/static/js/2.1a1355e5.chunk.js"
+  },
+  {
+    "revision": "22ee8baddb42141e24b6",
+    "url": "/findamovie/static/css/main.dece3029.chunk.css"
+  },
+  {
+    "revision": "971db2d00c6252d5f79623a99dd684b9",
+    "url": "/findamovie/index.html"
+  }
+];
